4cba99d4fd5a1c0eb68d39fc85a3b1eb6e13c3be
Anton Sych <anton.sych3@gmail.com>
2021-01-31 00:32:56 +0600

    Write all endpoints for controller

--- src/main/java/ua/com/clm/filestorage/controller/FileStorageController.java
+++ src/main/java/ua/com/clm/filestorage/controller/FileStorageController.java
@@ -1,72 +1,72 @@
 package ua.com.clm.filestorage.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.com.clm.filestorage.dto.FileRequestDto;
+import ua.com.clm.filestorage.dto.FileResponseDto;
 import ua.com.clm.filestorage.dto.FilesResponseDto;
-import ua.com.clm.filestorage.dto.UploadFileRequestDto;
-import ua.com.clm.filestorage.dto.UploadFileResponseDto;
 import ua.com.clm.filestorage.dto.ResponseDto;
-import ua.com.clm.filestorage.exception.BadRequestException;
-import ua.com.clm.filestorage.exception.FileNotFoundException;
-
-import java.util.Arrays;
+import ua.com.clm.filestorage.handler.CustomRequestHandler;
+import ua.com.clm.filestorage.model.BaseFile;
+import ua.com.clm.filestorage.model.File;
+import ua.com.clm.filestorage.sercice.FileService;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/file")
 public class FileStorageController {
 
+    private final CustomRequestHandler requestHeader;
+    private final FileService fileService;
+
     @GetMapping("")
-    public ResponseEntity<FilesResponseDto> getFiles(@RequestParam(defaultValue = "", required = false) String[] tags,
-                                                     @RequestParam(defaultValue = "0", required = false) int page,
-                                                     @RequestParam(defaultValue = "10", required = false) int size) {
-        System.out.println(Arrays.toString(tags) + " " + page + " " + size);
-        return new ResponseEntity<>(new FilesResponseDto(), HttpStatus.OK);
+    @ResponseBody
+    public FilesResponseDto getFiles(@RequestParam(defaultValue = "", required = false) String[] tags,
+                                     @RequestParam(defaultValue = "0", required = false) int page,
+                                     @RequestParam(defaultValue = "10", required = false) int size) {
+        log.info("REQUEST TO GET FILES BY TAG(-S) - {}", tags);
+        FilesResponseDto filesResponseDto = fileService.getFilesByTags(tags, page, size);
+        log.info("GOT {} FILES FOR REQUEST BY TAG(-S) - {}", filesResponseDto.getTotal(), tags);
+        return filesResponseDto;
     }
 
 
     @PostMapping("")
-    public ResponseEntity<UploadFileResponseDto> uploadFile(@RequestBody UploadFileRequestDto uploadFileRequestDto) {
-        log.info("REQUEST TO UPLOAD FILE - {}", uploadFileRequestDto.getName());
-        if (uploadFileRequestDto.getSize() == 1) {
-            log.error("Bad request body {}", uploadFileRequestDto);
-            throw new BadRequestException("bad request");
-        }
-        return new ResponseEntity<>(new UploadFileResponseDto(uploadFileRequestDto.getName()), HttpStatus.OK);
+    @ResponseBody
+    public BaseFile uploadFile(@RequestBody FileRequestDto fileRequestDto) {
+        log.info("REQUEST TO UPLOAD FILE - {}", fileRequestDto.getName());
+        requestHeader.checkRequest(fileRequestDto);
+        File uploadedFile = fileService.uploadFile(new File(fileRequestDto));
+        log.info("FILE WITH ID = {} HAS BEEN UPLOADED TO THE STORAGE", uploadedFile.getId());
+        return new FileResponseDto(uploadedFile.getId());
     }
 
     @DeleteMapping("/{ID}")
-    public ResponseEntity<ResponseDto> deleteFile(@PathVariable("ID") String id) {
+    @ResponseBody
+    public ResponseDto deleteFile(@PathVariable("ID") String id) {
         log.info("REQUEST TO DELETE FILE WITH ID = {}", id);
-        if (id.equals("1")) {
-            log.error("File with id = {} is not found", id);
-            throw new FileNotFoundException("file not found");
-        }
-        return new ResponseEntity<>(new ResponseDto(true), HttpStatus.OK);
+        fileService.deleteFileById(id);
+        log.info("DELETED FILE WITH ID = {}", id);
+        return new ResponseDto(true);
     }
 
     @PostMapping("/{ID}/tags")
-    public ResponseEntity<ResponseDto> assignTags(@RequestBody String[] tags, @PathVariable("ID") String id) {
-        log.info("REQUEST TO ASSIGN TAGS FOR FILE WITH ID = {}", id);
-        if (id.equals("1")) {
-            log.error("File with id = {} is not found", id);
-            throw new FileNotFoundException("file not found");
-        }
-        return new ResponseEntity<>(new ResponseDto(true), HttpStatus.OK);
+    @ResponseBody
+    public ResponseDto assignTags(@RequestBody String[] tags, @PathVariable("ID") String id) {
+        log.info("REQUEST TO ASSIGN TAGS - {} FOR FILE WITH ID = {}", tags, id);
+        fileService.assignTags(tags, id);
+        log.info("ASSIGNED TAGS - {} ON THE FILE WITH ID = {}", tags, id);
+        return new ResponseDto(true);
     }
 
     @DeleteMapping("/{ID}/tags")
-    public ResponseEntity<ResponseDto> removeTags(@RequestBody String[] tags, @PathVariable("ID") String id) {
-        log.info("REQUEST TO REMOVE TAGS FOR FILE WITH ID = {}", id);
-        if (id.equals("1")) {
-            log.error("Some tag is not found on the file with id = {}", id);
-            throw new BadRequestException("tag not found on file");
-        } else if (id.equals("2")) {
-            log.error("File with id = {} is not found", id);
-            throw new FileNotFoundException("file not found");
-        }
-        return new ResponseEntity<>(new ResponseDto(true), HttpStatus.OK);
+    @ResponseBody
+    public ResponseDto removeTags(@RequestBody String[] tags, @PathVariable("ID") String id) {
+        log.info("REQUEST TO REMOVE TAGS - {} FOR FILE WITH ID = {}", tags, id);
+        fileService.removeTags(tags, id);
+        log.info("REMOVED TAGS - {} FROM FILE WITH ID = {}", tags, id);
+        return new ResponseDto(true);
     }
 }
