d8096adc6c875767101234abfae6f3cfd9619ab6
Anton Sych <anton.sych3@gmail.com>
2021-01-31 16:49:44 +0600

    Add method for default tags

--- src/main/java/ua/com/clm/filestorage/sercice/FileServiceImpl.java
+++ src/main/java/ua/com/clm/filestorage/sercice/FileServiceImpl.java
@@ -10,9 +10,12 @@ import ua.com.clm.filestorage.exception.BadRequestException;
 import ua.com.clm.filestorage.exception.FileNotFoundException;
 import ua.com.clm.filestorage.model.File;
 import ua.com.clm.filestorage.repository.FileRepository;
+import ua.com.clm.filestorage.type.BaseTag;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -21,11 +24,25 @@ public class FileServiceImpl implements FileService {
 
     private static final String FILE_NOT_FOUND = "file not found";
     private static final String TAG_NOT_FOUND = "tag not found on file";
+    private static final Map<String, Enum<BaseTag>> ALL_EXTENSIONS = new HashMap<>();
+
+    static {
+        ALL_EXTENSIONS.put("mp3", BaseTag.AUDIO);
+        ALL_EXTENSIONS.put("wav", BaseTag.AUDIO);
+        ALL_EXTENSIONS.put("mp4", BaseTag.VIDEO);
+        ALL_EXTENSIONS.put("avi", BaseTag.VIDEO);
+        ALL_EXTENSIONS.put("doc", BaseTag.DOCUMENT);
+        ALL_EXTENSIONS.put("pdf", BaseTag.DOCUMENT);
+        ALL_EXTENSIONS.put("txt", BaseTag.DOCUMENT);
+        ALL_EXTENSIONS.put("jpg", BaseTag.IMAGE);
+        ALL_EXTENSIONS.put("png", BaseTag.IMAGE);
+    }
 
     private final FileRepository fileRepository;
 
+    @Override
     public File uploadFile(File file) {
-        return fileRepository.save(file);
+        return fileRepository.save(addAccordingTags(file));
     }
 
     @Override
@@ -76,6 +93,7 @@ public class FileServiceImpl implements FileService {
                 .orElseThrow(() -> new FileNotFoundException(FILE_NOT_FOUND));
     }
 
+    @Override
     public FilesResponseDto getFilesByTags(List<String> tags, int page, int size) {
         FilesResponseDto filesResponseDto = new FilesResponseDto();
         Page<File> pageableFiles;
@@ -88,5 +106,17 @@ public class FileServiceImpl implements FileService {
         filesResponseDto.setTotal((int) pageableFiles.getTotalElements());
         filesResponseDto.setFiles(pageableFiles.getContent());
         return filesResponseDto;
+    }
+
+    private File addAccordingTags(File file) {
+        String[] nameArray = file.getName().trim().split("\\.");
+        String extension = nameArray[nameArray.length - 1];
+        List<String> tags = new ArrayList<>();
+        BaseTag tag = (BaseTag) ALL_EXTENSIONS.get(extension);
+        if (tag != null) {
+            tags.add(tag.getName());
+            file.setTags(tags);
+        }
+        return file;
     }
 }
